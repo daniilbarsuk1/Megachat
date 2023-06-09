5e69876eae1db7df7f4f848323a5d5fac0d0fec4
Daniil Barsuk <daniil.barsuk@sqilsoft.by>
2023-06-09 15:24:11 +0300

    PRSUMMER-11 Initial Commit

---
+++ src/main/java/com/sqilsoft/megachat/MegachatApplication.java
@@ -0,0 +1,13 @@
+package com.sqilsoft.megachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegachatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqilsoft/megachat/MegachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqilsoft.megachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
